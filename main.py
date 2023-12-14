050f64e375ec467b0cb23e2997c52d0cd80d02a8
Arthur-king18 <arturik060406@gmail.com>
2023-12-14 21:03:36 +0400

    create program

--- main.py
+++ main.py
@@ -1,145 +1,115 @@
-# import face_recognition
-# import cv2
-# import numpy as np
-# from datetime import datetime
-#
-# from config import known_face_encodings, known_face_names
-#
-# video_capture = cv2.VideoCapture(0)
-#
-# # Initialize some variables
-# face_locations = []
-# face_encodings = []
-# face_names = []
-#
-#
-# def main():
-#     process_this_frame = True
-#     while True:
-#         # Grab a single frame of video
-#         ret, frame = video_capture.read()
-#
-#         # Only process every other frame of video to save time
-#         if process_this_frame:
-#             # Resize frame of video to 1/4 size for faster face recognition processing
-#             small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-#
-#             # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-#             rgb_small_frame = small_frame[:, :, ::-1]
-#
-#             # Find all the faces and face encodings in the current frame of video
-#             face_locations = face_recognition.face_locations(rgb_small_frame)
-#             face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-#
-#             face_names = []
-#             for face_encoding in face_encodings:
-#                 # See if the face is a match for the known face(s)
-#                 matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-#                 name = "Unknown"
-#
-#
-#
-#
-#                 # # If a match was found in known_face_encodings, just use the first one.
-#                 # if True in matches:
-#                 #     first_match_index = matches.index(True)
-#                 #     name = known_face_names[first_match_index]
-#
-#                 # Or instead, use the known face with the smallest distance to the new face
-#                 face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-#                 best_match_index = np.argmin(face_distances)
-#                 if matches[best_match_index]:
-#                     name = known_face_names[best_match_index]
-#
-#                 # print(name)
-#                 face_names.append(name)
-#                 if name != 'Unknown':
-#                     print(f'Обнаружен(а) {name} ')
-#                     # markAttendance(name=name)
-#                 else:
-#                     print('Неизвестный обЪект')
-#
-#
-#                     # engine.runAndWait()
-#
-#         process_this_frame = not process_this_frame
-#         #
-#         # # Display the results
-#         for (top, right, bottom, left), name in zip(face_locations, face_names):
-#             # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-#             top *= 4
-#             right *= 4
-#             bottom *= 4
-#             left *= 4
-#
-#             # Draw a box around the face
-#             cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-#
-#             # Draw a label with a name below the face
-#             cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-#             font = cv2.FONT_HERSHEY_DUPLEX
-#             cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-#
-#         # Display the resulting image
-#         cv2.imshow('Video', frame)
-#
-#         # Hit 'q' on the keyboard to quit!
-#         if cv2.waitKey(1) & 0xFF == ord('q'):
-#             break
-#
-#
-#     video_capture.release()
-#     cv2.destroyAllWindows()
-#
-#
-# def markAttendance(name):
-#     with open('Attendance.csv', 'r+') as f:
-#         myDataList = f.readlines()
-#         nameList = []
-#         for line in myDataList:
-#             entry = line.split(',')
-#             nameList.append(entry[0])
-#         if name not in nameList:
-#             now = datetime.now()
-#             dtString = now.strftime('%H:%M:%S')
-#             f.writelines(f'\n{name},{dtString}')
-#
-#
-#
-#
-#
-# if __name__ == '__main__':
-#     main()
-#
+import os
 
 import face_recognition
+import cv2
+import numpy
+import numpy as np
+from datetime import datetime
 
-# Load a sample picture and learn how to recognize it.
+import pygame
+
+
 ilon_image = face_recognition.load_image_file("ImagesAttendance/ilon.jpg")
 ilon_face_encoding = face_recognition.face_encodings(ilon_image)[0]
 
-# # Load a second sample picture and learn how to recognize it.
-# biden_image = face_recognition.load_image_file("biden.jpg")
-# biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
-#
-# l_image = face_recognition.load_image_file("l.jpg")
-# l_face_encoding = face_recognition.face_encodings(l_image)[0]
-#
-# d_image = face_recognition.load_image_file("d.jpg")
-# d_face_encoding = face_recognition.face_encodings(d_image)[0]
-
-
-
-# Create arrays of known face encodings and their names
 known_face_encodings = [
     ilon_face_encoding
-    # biden_face_encoding,
-    # l_face_encoding
 
 ]
 known_face_names = [
     "ELON"
-    # 'Biden',
-    # 'Alexey',
-    # 'Dasha'
 ]
+
+
+video_capture = cv2.VideoCapture(0)
+
+face_locations = []
+face_encodings = []
+face_names = []
+
+
+def main():
+    process_this_frame = True
+    while True:
+        ret, frame = video_capture.read()
+
+        # Обрабатывает только каждый второй кадр видео, чтобы сэкономить время
+        if process_this_frame:
+            # Изменяет размер кадра видео до 1/4 для более быстрой обработки распознавания лиц.
+            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+            # Преобразование изображения из цвета BGR (который использует OpenCV) в цвет RGB (который использует face_recognition)
+            rgb_small_frame = rgb_small_frame = numpy.ascontiguousarray(small_frame[:, :, ::-1])
+
+            # Находит все лица и кодировки лиц в текущем кадре видео.
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+            face_names = []
+            for face_encoding in face_encodings:
+                # Проверяет, соответствует ли лицо известному лицу (лицам)
+                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+                name = "Unknown"
+
+                # Или вместо этого использует известное лицо с наименьшим расстоянием до нового лица.
+                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = known_face_names[best_match_index]
+
+                face_names.append(name)
+                if name != 'Unknown':
+                    print(f'Обнаружен(а) {name} ')
+                    markAttendance(name=name)
+
+                else:
+                    markAttendance(name=name)
+                    cv2.imwrite(f"{datetime.utcnow()}.jpg", frame)
+                    print('Неизвестный обЪект')
+                    pygame.mixer.init()
+                    pygame.mixer.music.load("c7b68d9df6de3c5.mp3")
+                    pygame.mixer.music.play()
+
+        process_this_frame = not process_this_frame
+
+        # Отображение результатов - только для показа. На микрокомпьютере не запускаю функци для экономии ресурсов
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            # Масштабируем обратное расположение лиц, поскольку обнаруженный нами кадр был уменьшен до 1/4 размера.
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+            # Нарисуем рамку вокруг лица
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+            # Нарисуем метку с именем под лицом
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+        # Отобрази полученное изображение
+        cv2.imshow('Video', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+
+def markAttendance(name):
+    with open('Attendance.csv', 'r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(name)
+        if name not in nameList:
+            now = datetime.utcnow()
+            f.writelines(f'\n{name} - {now}')
+
+
+if __name__ == '__main__':
+    main()
+
