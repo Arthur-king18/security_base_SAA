7555deb9695d74509b8b38f20603e127f569c44e
Arthur-king18 <arturik060406@gmail.com>
2023-12-15 21:24:52 +0400

    releas

--- main.py
+++ main.py
@@ -73,23 +73,23 @@ def main():
         process_this_frame = not process_this_frame
 
         # Отображение результатов - только для показа. На микрокомпьютере не запускаю функци для экономии ресурсов
-        for (top, right, bottom, left), name in zip(face_locations, face_names):
-            # Масштабируем обратное расположение лиц, поскольку обнаруженный нами кадр был уменьшен до 1/4 размера.
-            top *= 4
-            right *= 4
-            bottom *= 4
-            left *= 4
-
-            # Нарисуем рамку вокруг лица
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-            # Нарисуем метку с именем под лицом
-            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-
-        # Отобрази полученное изображение
-        cv2.imshow('Video', frame)
+        # for (top, right, bottom, left), name in zip(face_locations, face_names):
+        #     # Масштабируем обратное расположение лиц, поскольку обнаруженный нами кадр был уменьшен до 1/4 размера.
+        #     top *= 4
+        #     right *= 4
+        #     bottom *= 4
+        #     left *= 4
+        #
+        #     # Нарисуем рамку вокруг лица
+        #     cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        #
+        #     # Нарисуем метку с именем под лицом
+        #     cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        #     font = cv2.FONT_HERSHEY_DUPLEX
+        #     cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+        #
+        # # Отобрази полученное изображение
+        # cv2.imshow('Video', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
